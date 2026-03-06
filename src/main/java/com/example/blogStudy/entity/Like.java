21416fe35b9c9947dae8d3852f1c85bbb0b02cec
KymCat <kohina1452@gmail.com>
2026-03-06 18:15:12 +0900

    Like Entity 작성

---
+++ src/main/java/com/example/blogStudy/entity/Like.java
@@ -0,0 +1,44 @@
+package com.example.blogStudy.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+        name = "likes",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"user_id", "post_id"})
+        }
+)
+public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "like_id")
+    private Long id;
+
+    @Column(name = "like_date")
+    private LocalDateTime likeDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+
+    @Builder
+    public Like(User user, Post post) {
+        this.likeDate = LocalDateTime.now();
+        this.user = user;
+        this.post = post;
+    }
+}
