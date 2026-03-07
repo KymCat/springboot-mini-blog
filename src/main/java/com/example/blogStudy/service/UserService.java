eca1ade5d9d43d7cc156930be2140c9126836f4f
KymCat <kohina1452@gmail.com>
2026-03-07 16:39:16 +0900

    UserService 사용하지않는 import 삭제

--- src/main/java/com/example/blogStudy/service/UserService.java
+++ src/main/java/com/example/blogStudy/service/UserService.java
@@ -7,8 +7,6 @@ import com.example.blogStudy.entity.User;
 import com.example.blogStudy.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
