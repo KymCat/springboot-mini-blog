607523c0457dfba97c6128e9becb562a7ebef07e
KymCat <kohina1452@gmail.com>
2026-03-09 16:23:13 +0900

    .collect(Collectors.toList()) -> .toList() 로 수정

--- src/main/java/com/example/blogStudy/service/UserService.java
+++ src/main/java/com/example/blogStudy/service/UserService.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -23,7 +22,7 @@ public class UserService {
     public List<UserResponseDto> getUsers() {
         return userRepository.findAll().stream()
                 .map(UserResponseDto::from)
-                .collect(Collectors.toList());
+                .toList();
     }
 
     // 유저 한명 조회
@@ -33,7 +32,6 @@ public class UserService {
                 .orElseThrow(() -> new EntityNotFoundException("User Not Found : " + id));
 
         return UserResponseDto.from(user);
-
     }
 
     // 유저 계정 생성
