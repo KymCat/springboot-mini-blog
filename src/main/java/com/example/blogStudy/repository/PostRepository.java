b8b0a0ddea4a4e148849b0c57a2e0cdf44d165bd
KymCat <kohina1452@gmail.com>
2026-03-10 19:25:39 +0900

    주석 추가

--- src/main/java/com/example/blogStudy/repository/PostRepository.java
+++ src/main/java/com/example/blogStudy/repository/PostRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    // 게시글 전체 조회 N+1 fetch join 해결 방법
     @Query("SELECT p FROM Post p JOIN FETCH p.user")
     List<Post> findAllWithUser();
 
