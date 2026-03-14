cf489bd269f3ed90493ea915276bd5298ca9f603
KymCat <kohina1452@gmail.com>
2026-03-14 17:07:20 +0900

    Jwt-Redis-refreshToken 테스트 코드

--- src/main/java/com/example/blogStudy/controller/RedisTestController.java
+++ src/main/java/com/example/blogStudy/controller/RedisTestController.java
@@ -1,20 +1,38 @@
 package com.example.blogStudy.controller;
 
+import com.example.blogStudy.service.RefreshTokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/redis-test")
 public class RedisTestController {
-    private final StringRedisTemplate stringRedisTemplate;
 
-    @GetMapping("/redis-test")
-    public String redisTest() {
-        stringRedisTemplate.opsForValue().set("test:key", "hello world",15, TimeUnit.SECONDS); // 15초 뒤 만료
-        return stringRedisTemplate.opsForValue().get("test:key");
+    private final RefreshTokenService refreshTokenService;
+
+    @PostMapping("/save")
+    public String save() {
+        refreshTokenService.save("kim123", "sample-refresh-token", 60000);
+        return "saved";
+    }
+
+    @GetMapping("/get")
+    public String get() {
+        return refreshTokenService.findByUserId("kim123");
+    }
+
+    @GetMapping("/exists")
+    public boolean exists() {
+        return refreshTokenService.exists("kim123");
+    }
+
+    @DeleteMapping("/delete")
+    public String delete() {
+        refreshTokenService.delete("kim123");
+        return "deleted";
     }
 }
---
+++ src/main/java/com/example/blogStudy/service/RefreshTokenService.java
@@ -0,0 +1,45 @@
+package com.example.blogStudy.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshTokenService {
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    private String createKey(String userId) {
+        return "refresh:" + userId;
+    }
+
+    // refresh 토큰 저장
+    public void save(String userId, String refreshToken, long expireTime) {
+        stringRedisTemplate.opsForValue().set(
+                createKey(userId),  // key
+                refreshToken,       // value
+                expireTime,         // TTL
+                TimeUnit.MILLISECONDS
+                );
+    }
+
+    // 해당 유저의 refresh 토큰(value) 조회
+    public String findByUserId(String userId) {
+        return stringRedisTemplate.opsForValue().get(createKey(userId));
+    }
+
+    // 해당 유저의 refresh 토큰 존재 여부 확인
+    public boolean exists(String userId) {
+        Boolean hasKey = stringRedisTemplate.hasKey(createKey(userId));
+        return Boolean.TRUE.equals(hasKey);
+    }
+
+    // 로그아웃할 때 삭제
+    public void delete(String userId) {
+        stringRedisTemplate.delete(createKey(userId));
+    }
+
+}
