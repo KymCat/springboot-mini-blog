713133c7b6472d427220ba7d092b6fdc2177992c
KymCat <kohina1452@gmail.com>
2026-03-19 17:53:01 +0900

    Like CRUD에 인증 사용자 정보 적용하도록 수정

--- src/main/java/com/example/blogStudy/controller/LikeController.java
+++ src/main/java/com/example/blogStudy/controller/LikeController.java
@@ -3,6 +3,7 @@ package com.example.blogStudy.controller;
 import com.example.blogStudy.service.LikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,14 +17,18 @@ public class LikeController {
 
     // 해당 게시글 좋아요 여부
     @GetMapping("/posts/{postId}/likes")
-    ResponseEntity<Boolean> likes(@PathVariable Long postId) {
-        return ResponseEntity.ok(likeService.likes(postId));
+    ResponseEntity<Boolean> likes(Authentication auth, @PathVariable Long postId) {
+
+        String userId = auth.getName();
+        return ResponseEntity.ok(likeService.likes(userId, postId));
     }
 
     // 게시글 좋아요 생성
     @PostMapping("/posts/{postId}/likes")
-    ResponseEntity<Void> likePost(@PathVariable Long postId) {
-        likeService.likePost(postId);
+    ResponseEntity<Void> likePost(Authentication auth, @PathVariable Long postId) {
+
+        String userId = auth.getName();
+        likeService.likePost(userId, postId);
 
         return ResponseEntity.ok().build();
     }
--- src/main/java/com/example/blogStudy/service/LikeService.java
+++ src/main/java/com/example/blogStudy/service/LikeService.java
@@ -7,6 +7,7 @@ import com.example.blogStudy.exception.CustomException;
 import com.example.blogStudy.exception.ErrorCode;
 import com.example.blogStudy.repository.LikeRepository;
 import com.example.blogStudy.repository.PostRepository;
+import com.example.blogStudy.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class LikeService {
     private final LikeRepository likeRepository;
+    private final UserRepository userRepository;
     private final PostRepository postRepository;
 
     // 세션 (임시)
@@ -24,17 +26,20 @@ public class LikeService {
             "김민수");
 
     // 해당 게시글 좋아요 여부
-    public Boolean likes(Long postId) {
-        return likeRepository.existsByUserIdAndPostId(session.getId(), postId);
+    public Boolean likes(String userId, Long postId) {
+        return likeRepository.existsByUserIdAndPostId(userId, postId);
     }
 
     // 게시글 좋아요
     @Transactional
-    public void likePost(Long postId) {
+    public void likePost(String userId, Long postId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
         Post post =  postRepository.findById(postId)
                 .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
 
-        Like like = Like.create(session, post);
+        Like like = Like.create(user, post);
         likeRepository.save(like);
     }
 
