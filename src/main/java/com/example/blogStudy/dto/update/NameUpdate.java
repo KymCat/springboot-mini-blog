e56070a5442bcad6f951f1d144ddcdfff761892d
KymCat <kohina1452@gmail.com>
2026-03-25 18:10:36 +0900

    정규식 및 메세지 수정

--- src/main/java/com/example/blogStudy/dto/update/NameUpdate.java
+++ src/main/java/com/example/blogStudy/dto/update/NameUpdate.java
@@ -10,7 +10,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public class NameUpdate {
 
-    @NotBlank(message = "이름을 입력해주세요.")
+    @NotBlank(message = "닉네임을 입력해주세요.")
     @Size(min = 2, max = 30, message = "닉네임은 2자 이상 30자 이하로 입력해주세요.")
     @Pattern(regexp = "^[a-zA-Z0-9가-힣]+$", message = "닉네임은 한글, 영어 소문자, 숫자만 가능합니다.")
     private String name;
--- src/main/java/com/example/blogStudy/dto/update/PasswordUpdate.java
+++ src/main/java/com/example/blogStudy/dto/update/PasswordUpdate.java
@@ -12,11 +12,17 @@ public class PasswordUpdate {
 
     @NotBlank(message = "비밀번호를 입력해주세요.")
     @Size(min = 8, max = 15, message = "비밀번호는 8자 이상 15자 이하로 입력해주세요.")
-    @Pattern(regexp = "^\\S+$", message = "비밀번호에 공백은 포함될 수 없습니다.")
+    @Pattern(
+            regexp = "^(?=.*[a-zA-Z])(?=.*\\d)[a-zA-Z\\d!@#$%^&*()_+=-]+$",
+            message = "비밀번호에 공백은 포함될 수 없습니다."
+    )
     private String currentPassword;
 
     @NotBlank(message = "비밀번호를 입력해주세요.")
     @Size(min = 8, max = 15, message = "비밀번호는 8자 이상 15자 이하로 입력해주세요.")
-    @Pattern(regexp = "^\\S+$", message = "비밀번호에 공백은 포함될 수 없습니다.")
+    @Pattern(
+            regexp = "^(?=.*[a-zA-Z])(?=.*\\d)[a-zA-Z\\d!@#$%^&*()_+=-]+$",
+            message = "비밀번호에 공백은 포함될 수 없습니다."
+    )
     private String newPassword;
 }
