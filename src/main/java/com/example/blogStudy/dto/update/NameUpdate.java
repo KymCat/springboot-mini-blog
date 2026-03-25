d37f89415c97c6d7c54f18e4cc9896fa58793b19
KymCat <kohina1452@gmail.com>
2026-03-25 17:51:10 +0900

    닉네임 영문 대문자도 허용하도록 수정

--- src/main/java/com/example/blogStudy/dto/update/NameUpdate.java
+++ src/main/java/com/example/blogStudy/dto/update/NameUpdate.java
@@ -12,6 +12,6 @@ public class NameUpdate {
 
     @NotBlank(message = "이름을 입력해주세요.")
     @Size(min = 2, max = 30, message = "닉네임은 2자 이상 30자 이하로 입력해주세요.")
-    @Pattern(regexp = "^[a-z0-9가-힣]+$", message = "닉네임은 한글, 영어 소문자, 숫자만 가능합니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9가-힣]+$", message = "닉네임은 한글, 영어 소문자, 숫자만 가능합니다.")
     private String name;
 }
