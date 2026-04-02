5b34c44849caf2e8ed5e1338a9d47c962be9ecb9
KymCat <kohina1452@gmail.com>
2026-04-02 18:58:32 +0900

    AuthController @WebMvcTest 토큰 재발행 테스트 코드 구현

--- src/test/java/com/example/blogStudy/auth/AuthControllerTest.java
+++ src/test/java/com/example/blogStudy/auth/AuthControllerTest.java
@@ -3,6 +3,7 @@ package com.example.blogStudy.auth;
 import com.example.blogStudy.dto.request.LoginRequest;
 import com.example.blogStudy.jwt.JwtTokenResult;
 import com.example.blogStudy.security.JwtAuthenticationFilter;
+import jakarta.servlet.http.Cookie;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,7 @@ import tools.jackson.databind.ObjectMapper;
 import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.containsString;
 
 @WebMvcTest(
         controllers = AuthController.class,
@@ -61,7 +63,12 @@ class AuthControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string("access-token-value"))
                 .andExpect(header().string(HttpHeaders.SET_COOKIE,
-                        org.hamcrest.Matchers.containsString("refreshToken=refresh-token-value")));
+                        containsString("refreshToken=refresh-token-value")))
+                .andExpect(header().string(HttpHeaders.SET_COOKIE,
+                        containsString("HttpOnly")))
+                .andExpect(header().string(HttpHeaders.SET_COOKIE,
+                        containsString("SameSite=Lax")));
+
     }
 
     @Test
@@ -104,6 +111,91 @@ class AuthControllerTest {
                 .andExpect(jsonPath("$.path").value("/auth/login"));
     }
 
+    @Test
+    @DisplayName("로그아웃 성공")
+    void logout_success() throws Exception {
+        // given
+        String refreshToken = "refresh-token-value";
 
+        // when
+        ResultActions result = mockMvc.perform(post("/auth/logout")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer access-token-value")
+                .cookie(new Cookie("refreshToken", refreshToken)));
 
+        // then
+        result
+                .andExpect(status().isOk())
+                .andExpect(header().string(HttpHeaders.SET_COOKIE,
+                        containsString("")))
+                .andExpect(header().string(HttpHeaders.SET_COOKIE,
+                        containsString("Max-Age=0")));
+
+        verify(authService).logout("access-token-value", refreshToken);
+    }
+
+    @Test
+    @DisplayName("로그아웃 실패 : Authorization Header null")
+    void logout_fail_header_null()  throws Exception {
+        // given
+        String refreshToken = "refresh-token-value";
+
+        // when
+        ResultActions result = mockMvc.perform(post("/auth/logout")
+                .cookie(new Cookie("refreshToken", refreshToken)));
+
+        // then
+        result
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.status").value(401))
+                .andExpect(jsonPath("$.code").value("AUTH-001"))
+                .andExpect(jsonPath("$.message").exists())
+                .andExpect(jsonPath("$.path").value("/auth/logout"));
+    }
+
+    @Test
+    @DisplayName("토큰 재발행 성공")
+    void reissue_success() throws Exception {
+        // given
+        String accessToken = "access-token-value";
+        String refreshToken = "refresh-token-value";
+        JwtTokenResult jwtTokenResult = new JwtTokenResult(
+                accessToken,
+                refreshToken,
+                600000L
+        );
+
+        given(authService.reissue(any(String.class))).willReturn(jwtTokenResult);
+
+        // when
+        ResultActions result = mockMvc.perform(post("/auth/reissue")
+                .cookie(new Cookie("refreshToken", refreshToken)));
+
+        // then
+        result
+                .andExpect(status().isOk())
+                .andExpect(header().string(HttpHeaders.SET_COOKIE,
+                        containsString("refreshToken="+refreshToken)))
+                .andExpect(header().string(HttpHeaders.SET_COOKIE,
+                        containsString("HttpOnly")))
+                .andExpect(header().string(HttpHeaders.SET_COOKIE,
+                        containsString("SameSite=Lax")));
+    }
+
+    @Test
+    @DisplayName("토큰 재발행 실패 : 쿠키 없음")
+    void reissue_fail_refresh_token() throws Exception {
+        //given
+        // refresh-token 쿠키 없이 요청
+
+        // when
+        ResultActions result = mockMvc.perform(post("/auth/reissue"));
+
+        // then
+        result
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value(400))
+                .andExpect(jsonPath("$.code").value("AUTH-003"))
+                .andExpect(jsonPath("$.message").exists())
+                .andExpect(jsonPath("$.path").value("/auth/reissue"));
+    }
 }
